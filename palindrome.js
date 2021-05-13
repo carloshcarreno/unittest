3ea5fb9cfc353fbb81e21b340dda3414d1175faa
Carlos <carlosh.carreno@upb.edu.co>
2021-05-13 02:29:06 -0500

    Status check enabled 3.0

--- palindrome.js
+++ palindrome.js
@@ -8,7 +8,7 @@ function isAnagram(text1, text2){
     t1 = text1.toLowerCase().split('').sort().join('').trim();
     t2 = text2.toLowerCase().split('').sort().join('').trim();
 
-    return t1 === "hola";
+    return t1 === "";
 }
 
 module.exports = { isPalindrome, isAnagram};
